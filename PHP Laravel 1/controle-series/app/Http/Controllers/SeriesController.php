ea86e0ecd4842fc721a757c7c78e3eb801307efe
Diego Torres <torressdiiego@outlook.com>
2020-11-30 16:37:33 -0300

    a

--- PHP Laravel 1/controle-series/app/Http/Controllers/SeriesController.php
+++ PHP Laravel 1/controle-series/app/Http/Controllers/SeriesController.php
@@ -4,16 +4,21 @@
 namespace App\Http\Controllers;
 
 
+use App\Http\Requests\SeriesFormRequest;
 use App\Serie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use function Sodium\compare;
 
 class SeriesController extends Controller
 {
     public function index(Request $request)
     {
-        $series = Serie::all();
+        $series = Serie::query()->orderBy('nome')->get();
 
-        return view('series.index', compact('series'));
+        $mensagem = $request->session()->get('mensagem');
+
+        return view('series.index', compact('series', 'mensagem'));
     }
 
     public function create(Request $request)
@@ -21,12 +26,24 @@ class SeriesController extends Controller
         return view('series.create');
     }
 
-    public function store(Request $request)
+    public function store(SeriesFormRequest $request)
     {
         //$nome = $request->get('nome');
         $nome = $request->nome;
         $serie = new Serie();
         $serie->nome = $nome;
         $serie->save();
+
+        $request->session()->flash('mensagem', "Série {$serie->id} criada com sucesso");
+
+        return redirect()->route('listar_series');
+    }
+
+    public function destroy(Request $request)
+    {
+        Serie::destroy($request->id);
+        $request->session()->flash('mensagem', "Série removida com sucesso");
+
+        return redirect()->route('listar_series');
     }
 }
--- PHP Laravel 1/controle-series/resources/views/series/index.blade.php
+++ PHP Laravel 1/controle-series/resources/views/series/index.blade.php
@@ -5,11 +5,28 @@ Série
 @endsection
 
 @section('conteudo')
-    <a href="/series/criar" class="btn btn-dark mb-2">Adicionar</a>
+
+    @if(!session($mensagem))
+    <div class="alert alert-success">
+        {{$mensagem}}
+    </div>
+    @endif
+
+    <a href="{{ route('form_criar_serie') }}" class="btn btn-dark mb-2">Adicionar</a>
 
     <ul class="list-group">
         @foreach($series as $serie)
-            <li class="list-group-item">{{$serie->nome}}</li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">{{$serie->nome}}
+                <form action="/series/{{$serie->id}}" method="post" onsubmit="return confirm('Tem certeza?')">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger btn-sm">
+                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
+                        </svg>
+                    </button>
+                </form>
+            </li>
         @endforeach
     </ul>
 @endsection
